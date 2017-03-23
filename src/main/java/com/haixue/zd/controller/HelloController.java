7a474512584923681fbbf13879d9c0c6248ff8a5
844597709 <844597709@qq.com>
2017-03-23 17:52:27 +0800

    test merge--branch

--- src/main/java/com/haixue/zd/controller/HelloController.java
+++ src/main/java/com/haixue/zd/controller/HelloController.java
@@ -19,6 +19,6 @@ public class HelloController {
 
     public void change(){
         System.out.println("this is branch change info.");
-        System.out.println("merge");
+        System.out.println("merge complete...");
     }
 }
