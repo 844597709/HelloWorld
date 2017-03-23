7d7af0996d2f29c0959e506c71ad92a472dc3feb
844597709 <844597709@qq.com>
2017-03-23 13:04:17 +0800

    commit暂存区中文件

---
+++ src/main/java/com/haixue/zd/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.haixue.zd.controller;
+
+/**
+ * Created by zengdan on 2017/3/23.
+ */
+public class HelloController {
+    public static void main(String[] args) {
+        System.out.println("this is HelloController");
+    }
+}
